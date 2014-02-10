2a5a6a48495ea9e369eb8b2269c785559c92911a
satheeskumarm <sathees_km@spanservices.com>
2014-02-10 01:29:16 -0800

    UserList View Added.

--- PayProc.UI.Web/Controllers/UserManagement/UserInfoController.cs
+++ PayProc.UI.Web/Controllers/UserManagement/UserInfoController.cs
@@ -25,6 +25,24 @@ namespace PayProc.UI.Web.Controllers
         public ActionResult Index()
         {
             var userList = usrDetailsMgr.FindUserList(0, 15);
+            SelectListItem item = new SelectListItem();
+            List<SelectListItem> statusList = new List<SelectListItem>();
+
+            item.Text = "-- Select Status --";
+            item.Value = "0";
+            statusList.Add(item);
+
+            item = new SelectListItem();
+            item.Text = "Active";
+            item.Value = "1";
+            statusList.Add(item);
+
+            item = new SelectListItem();
+            item.Text = "Inactive";
+            item.Value = "2";
+            statusList.Add(item);
+
+            ViewBag.Status = new SelectList(statusList, "Value", "Text");
             return View(userList);
         }
     }
